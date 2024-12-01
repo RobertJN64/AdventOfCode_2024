f0389366c9e70e682f3466f03724cb01e083db67
RobertJN64 <robertjnies@gmail.com>
2024-12-01 00:04:46 -0500

    raw solution to day 1

--- Day01/day01a.py
+++ Day01/day01a.py
@@ -2,12 +2,28 @@ import util
 
 def main():
     with open("Day01/day01.txt") as f:
-        lines = [line.strip() for line in f.readlines()]
+        lines = [line.strip().split('   ') for line in f.readlines()]
     print(lines[0:10])
 
+    first = []
+    second = []
+    for a, b in lines:
+        a = int(a)
+        b = int(b)
+        first.append(a)
+        second.append(b)
+
+    first = sorted(first)
+    second = sorted(second)
 
     answer = 0
-    for line in lines:
-        answer += int(line)
+    for a, b in zip(first, second):
+        answer += abs(a-b)
+
+    #
+    #
+    # answer = 0
+    # for line in lines:
+    #     answer += int(line)
 
     print(answer)
--- Day01/day01b.py
+++ Day01/day01b.py
@@ -2,12 +2,19 @@ import util
 
 def main():
     with open("Day01/day01.txt") as f:
-        lines = [line.strip() for line in f.readlines()]
+        lines = [line.strip().split('   ') for line in f.readlines()]
     print(lines[0:10])
 
+    first = []
+    second = []
+    for a, b in lines:
+        a = int(a)
+        b = int(b)
+        first.append(a)
+        second.append(b)
 
     answer = 0
-    for line in lines:
-        answer += int(line)
+    for item in first:
+        answer += second.count(item) * item
 
     print(answer)

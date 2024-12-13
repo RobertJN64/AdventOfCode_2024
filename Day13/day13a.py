5a11f24c4f0cd3ec6118036c1a8b45dd943d2f42
RobertJN64 <robertjnies@gmail.com>
2024-12-13 00:17:41 -0500

    raw solves

--- Day13/day13a.py
+++ Day13/day13a.py
@@ -5,9 +5,25 @@ def main():
         lines = [line.strip() for line in f.readlines()]
     print(lines[0:10])
 
-
     answer = 0
-    for line in lines:
-        answer += int(line)
+
+    for i in range(0, len(lines), 4):
+        btna = list(map(int, lines[i].split(': X+')[1].split(', Y+')))
+        btnb = list(map(int, lines[i+1].split(': X+')[1].split(', Y+')))
+        prize = list(map(int, lines[i+2].split(': X=')[1].split(', Y=')))
+
+        best_cost = float('inf')
+        found = False
+
+        for a in range(0, 100):
+            for b in range(0, 100):
+                if prize[0] == btna[0] * a + btnb[0] * b and prize[1] == btna[1] * a + btnb[1] * b:
+                    if a * 3 + b < best_cost:
+                        best_cost = a * 3 + b
+                        found = True
+                    print('possible', a, b)
+
+        if found:
+            answer += best_cost
 
     print(answer)
--- Day13/day13b.py
+++ Day13/day13b.py
@@ -1,3 +1,7 @@
+from sympy.solvers import solve
+from sympy.abc import x, y
+import sympy as sp
+
 import util
 
 def main():
@@ -5,9 +9,22 @@ def main():
         lines = [line.strip() for line in f.readlines()]
     print(lines[0:10])
 
-
     answer = 0
-    for line in lines:
-        answer += int(line)
+
+    for i in range(0, len(lines), 4):
+        btna = list(map(int, lines[i].split(': X+')[1].split(', Y+')))
+        btnb = list(map(int, lines[i+1].split(': X+')[1].split(', Y+')))
+        prize = list(map(int, lines[i+2].split(': X=')[1].split(', Y=')))
+        prize[0] += 10000000000000
+        prize[1] += 10000000000000
+
+        eq1 = sp.Eq(x * btna[0] + y * btnb[0], prize[0])
+        eq2 = sp.Eq(x * btna[1] + y * btnb[1], prize[1])
+        output = solve([eq1, eq2], dict=True)
+        x_val = output[0][x]
+        y_val = output[0][y]
+
+        if int(x_val) == x_val and int(y_val) == y_val:
+            answer += x_val * 3 + y_val
 
     print(answer)

e24512bb7a3fc5ca991db143fa30469dfe63b538
RobertJN64 <robertjnies@gmail.com>
2024-12-20 01:50:15 -0500

    made parta fast, time to find a general-ish solution

--- Day20/day20a.py
+++ Day20/day20a.py
@@ -1,22 +1,4 @@
-import copy
 import util
-
-def quick_search(grid, start, goal):
-    visited = set()
-    queue = [(start[0], start[1], 0)]
-    best_cost = float('inf')
-    while queue:
-        x, y, cost = queue.pop(0)
-        if (x, y) == goal and cost < best_cost:
-            best_cost = cost
-            continue
-        visited.add((x, y))
-        for dx, dy in util.cardinal:
-            n_x = x + dx
-            n_y = y + dy
-            if not util.out_of_bounds(grid, n_x, n_y) and grid[n_y][n_x] != '#' and (n_x, n_y) not in visited:
-                queue.append((n_x, n_y, cost+1))
-    return best_cost
 
 def main():
     with open("Day20/day20.txt") as f:
@@ -31,24 +13,25 @@ def main():
             if c == 'S':
                 start = (x,y)
 
-    baseline = quick_search(grid, start, goal)
+    def constraint(new_x, new_y, _, __):
+        return grid[new_y][new_x] != '#'
+
+    costs = util.wavefront(grid, start, goal, constraint)
 
     count = 0
-
-    for y, row in enumerate(grid):
-        print(y, len(grid))
-        for x, c in enumerate(row):
-            if c == '#':
-                new_grid = copy.deepcopy(grid)
-                new_grid[y][x] = '.'
-                new_time = quick_search(new_grid, start, goal)
-                # if new_time < baseline:
-                #     print(f"Shortcut saves {baseline - new_time}")
-                # if baseline - new_time == 2:
-                #     count += 1
-                if baseline - new_time >= 100:
-                    count += 1
+    for y, row in enumerate(costs):
+        for x, cost_one in enumerate(row):
+            if cost_one == 0:
+                continue
+            for dx, dy in util.cardinal:
+                n_x = x + dx * 2
+                n_y = y + dy * 2
+                if not util.out_of_bounds(grid, n_x, n_y) and costs[n_y][n_x] != 0:
+                    cost_two = costs[n_y][n_x]
+                    if cost_one - cost_two > 2:
+                        #print("Found shortcut that saves", cost_one - cost_two - 2)
+                        if cost_one - cost_two - 2 >= 100:
+                            count += 1
 
     print("done")
     print(count)
-
--- Day20/day20b.py
+++ Day20/day20b.py
@@ -2,12 +2,36 @@ import util
 
 def main():
     with open("Day20/day20.txt") as f:
-        lines = [line.strip() for line in f.readlines()]
-    print(lines[0:10])
+        grid = [list(line.strip()) for line in f.readlines()]
 
+    goal = (0, 0)
+    start = (0, 0)
+    for y, row in enumerate(grid):
+        for x, c in enumerate(row):
+            if c == 'E':
+                goal = (x,y)
+            if c == 'S':
+                start = (x,y)
 
-    answer = 0
-    for line in lines:
-        answer += int(line)
+    def constraint(new_x, new_y, _, __):
+        return grid[new_y][new_x] != '#'
 
-    print(answer)
+    costs = util.wavefront(grid, start, goal, constraint)
+
+    count = 0
+    for y, row in enumerate(costs):
+        for x, cost_one in enumerate(row):
+            if cost_one == 0:
+                continue
+            for dx, dy in util.cardinal:
+                n_x = x + dx * 2
+                n_y = y + dy * 2
+                if not util.out_of_bounds(grid, n_x, n_y) and costs[n_y][n_x] != 0:
+                    cost_two = costs[n_y][n_x]
+                    if cost_one - cost_two > 2:
+                        #print("Found shortcut that saves", cost_one - cost_two - 2)
+                        if cost_one - cost_two - 2 >= 100:
+                            count += 1
+
+    print("done")
+    print(count)

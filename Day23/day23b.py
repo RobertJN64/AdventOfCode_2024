49dd0f4ef9af05d48712aac91a603111dc70c99a
RobertJN64 <robertjnies@gmail.com>
2024-12-23 00:07:55 -0500

    partA (i hate graph puzzles)

--- Day23/day23b.py
+++ Day23/day23b.py
@@ -2,12 +2,30 @@ import util
 
 def main():
     with open("Day23/day23.txt") as f:
-        lines = [line.strip() for line in f.readlines()]
+        lines = [line.strip().split('-') for line in f.readlines()]
     print(lines[0:10])
 
+    connections = {}
+    for k, v in lines:
+        if k not in connections:
+            connections[k] = []
+        if v not in connections:
+            connections[v] = []
+        connections[k].append(v)
+        connections[v].append(k)
 
-    answer = 0
-    for line in lines:
-        answer += int(line)
+    groups = set()
 
-    print(answer)
+    for k in connections:
+        if k.startswith('t'):
+            print('checking computer', k)
+        else:
+            continue
+
+        for conn in connections[k]:
+            for subconn in connections[conn]:
+                if k in connections[subconn]:
+                    #print(k, conn, subconn, 'valid')
+                    groups.add(tuple(sorted((k, conn, subconn))))
+    print(len(groups))
+

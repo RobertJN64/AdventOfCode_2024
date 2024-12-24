79bea4df4e2057a29ebee90ad5f65c00637403fe
RobertJN64 <robertjnies@gmail.com>
2024-12-24 01:00:20 -0500

    solved day24

--- Day24/day24a.py
+++ Day24/day24a.py
@@ -1,13 +1,51 @@
 import util
 
+class Gate:
+    def __init__(self, t, opa, opb, out):
+        self.opa = opa
+        self.opb = opb
+        self.t = t
+        self.out = out
+
+    def eval(self, wires):
+        if self.out in wires:
+            return False
+        if self.opa not in wires or self.opb not in wires:
+            return False
+        if self.t == 'AND':
+            wires[self.out] = wires[self.opa] and wires[self.opb]
+        if self.t == 'OR':
+            wires[self.out] = wires[self.opa] or wires[self.opb]
+        if self.t == 'XOR':
+            wires[self.out] = wires[self.opa] ^ wires[self.opb]
+        return True
+
 def main():
     with open("Day24/day24.txt") as f:
         lines = [line.strip() for line in f.readlines()]
     print(lines[0:10])
 
-
-    answer = 0
+    wires = {}
+    gates = []
     for line in lines:
-        answer += int(line)
+        if ':' in line:
+            wname, value = line.split(': ')
+            wires[wname] = int(value)
+        if '->' in line:
+            gate_data, out = line.split(' -> ')
+            opa, t, opb = gate_data.split(' ')
+            gates.append(Gate(t, opa, opb, out))
 
-    print(answer)
+    changed = True
+    while changed:
+        changed = False
+        for gate in gates:
+            changed = changed or gate.eval(wires)
+        print(wires)
+
+    print(wires)
+    value = 0
+    for k, v in wires.items():
+        if k.startswith('z'):
+            value += 2 ** int(k.removeprefix('z')) * v
+    print(value)
--- Day24/day24b.py
+++ Day24/day24b.py
@@ -1,13 +1,56 @@
 import util
 
+def gate_name(xy, num):
+    num_str = str(num)
+    if num < 10:
+        num_str = '0' + num_str
+    return xy + num_str
+
+def check_inputs(gate, opa, t, opb):
+    return gate[1] == t and (gate[0] == opa and gate[2] == opb or gate[2] == opa and gate[0] == opb)
+
+def search_and_remove(opa, t, opb, gates):
+    found = None
+    for gate in gates:
+        if check_inputs(gate, opa, t, opb):
+            found = gate
+            break
+    else:
+        print('No gate', opa, t, opb)
+    if found:
+        gates.remove(found)
+    return found
+
 def main():
-    with open("Day24/day24.txt") as f:
+    with open("Day24/day24_mod.txt") as f:
         lines = [line.strip() for line in f.readlines()]
     print(lines[0:10])
 
-
-    answer = 0
+    gates = []
+    max_wire_value = 0
     for line in lines:
-        answer += int(line)
+        if ':' in line:
+            wname = line.split(': ')[0][1:]
+            max_wire_value = max(max_wire_value, int(wname))
+        if '->' in line:
+            gate_data, out = line.split(' -> ')
+            opa, t, opb = gate_data.split(' ')
+            gates.append((opa, t, opb, out))
 
-    print(answer)
+    print(gates)
+    print(len(gates))
+    # start by finding parts of adder 0
+    prev_carry_wire = search_and_remove(gate_name('x', 0), 'AND', gate_name('y', 0), gates)
+
+    for num in range(1, max_wire_value):
+        print(num)
+        partial_sum_wire = search_and_remove(gate_name('x', num), 'XOR', gate_name('y', num), gates)
+        partial_carry_wire_a = search_and_remove(gate_name('x', num), 'AND', gate_name('y', num), gates)
+        final_sum_wire = search_and_remove(partial_sum_wire[3], 'XOR', prev_carry_wire[3], gates)
+        partial_carry_wire_b = search_and_remove(partial_sum_wire[3], 'AND', prev_carry_wire[3], gates)
+        final_carry_wire = search_and_remove(partial_carry_wire_a[3], 'OR', partial_carry_wire_b[3], gates)
+
+        assert final_sum_wire[3] == gate_name('z', num), final_sum_wire[3] + ' ' + gate_name('z', num)
+        prev_carry_wire = final_carry_wire
+
+    print(len(gates))

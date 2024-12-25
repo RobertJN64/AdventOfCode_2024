eeaa2e01b92be1bfc1680635899b2f9a1e1db78a
RobertJN64 <robertjnies@gmail.com>
2024-12-25 01:56:35 -0500

    day25 easy

--- Day25/day25.py
+++ Day25/day25.py
@@ -5,9 +5,44 @@ def main():
         lines = [line.strip() for line in f.readlines()]
     print(lines[0:10])
 
+    items = []
+    last_item = []
+    for row in lines:
+        if row == "":
+            items.append(last_item)
+            last_item = []
+        else:
+            last_item.append(row)
+    items.append(last_item)
+
+    keys = []
+    locks = []
+
+    for item in items:
+        cols = []
+        for cindex in range(0, 5):
+            count = 0
+            for row in item:
+                if row[cindex] == '#':
+                    count += 1
+            cols.append(count - 1)
+        if item[0] == '#####':
+            locks.append(cols)
+        else:
+            keys.append(cols)
+
+    print(locks)
+    print(keys)
 
     answer = 0
-    for line in lines:
-        answer += int(line)
+
+    for lock in locks:
+        for key in keys:
+            for cindex in range(0, 5):
+                if lock[cindex] + key[cindex] > 5:
+                    break
+            else:
+                answer += 1
 
     print(answer)
+
